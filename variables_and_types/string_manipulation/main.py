4df93e4fc320e516be808880c0c0adbf101e3cfb
abanawaf2003-debug <>
2026-01-22 02:24:35 +0000

    String manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,12 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+
+# استخراج العناصر بواسطة التقطيع
+dairy1 = grocery_items[0:4]      # 'milk'
+dairy2 = grocery_items[5:11]     # 'cheese'
+bakery1 = grocery_items[12:17]   # 'bread'
+
+# بناء الجملة باستخدام الربط
+print("We have dairy and bakery items: " 
+      + dairy1 + ", " 
+      + dairy2 + ", and " 
+      + bakery1 + " in aisle 5")
